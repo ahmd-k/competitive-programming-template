cf7364c6d97733c9ac54f7c39602a3c1d0adb4a9
Ahmed Khan <51062682+ahmd-k@users.noreply.github.com>
2023-02-11 19:30:36 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using pi = pair<int, int>;
+
+#define pb push_back
+#define mp make_pair
+
+const int inf = 1e9;
+
+void solve()
+{
+}
+
+void setIO(string s)
+{
+	freopen((s + ".in").c_str(), "r", stdin);
+	freopen((s + ".out").c_str(), "w", stdout);
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	setIO("main");
+
+	solve();
+
+	// For input with multiple test cases
+	// int T;
+	// cin >> T;
+	// while (T--)
+	// {
+	// 	solve();
+	// }
+}
